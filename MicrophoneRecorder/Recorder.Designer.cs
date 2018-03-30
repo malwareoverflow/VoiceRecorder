0643a64f9f00830ef3a82479147107fb1970d289
Taha Temuri <tahatemuri7@outlook.com>
2018-03-30 11:07:44 +0500

    code

---
+++ MicrophoneRecorder/Recorder.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace MicrophoneRecorder
+{
+    partial class Recorder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Recorder));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Extension = new System.Windows.Forms.Label();
+            this.Savedir = new System.Windows.Forms.TextBox();
+            this.Savename = new System.Windows.Forms.TextBox();
+            this.ExtensionTextBox = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.stopbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SavePath";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Name";
+            // 
+            // Extension
+            // 
+            this.Extension.AutoSize = true;
+            this.Extension.Location = new System.Drawing.Point(52, 205);
+            this.Extension.Name = "Extension";
+            this.Extension.Size = new System.Drawing.Size(53, 13);
+            this.Extension.TabIndex = 0;
+            this.Extension.Text = "Extension";
+            // 
+            // Savedir
+            // 
+            this.Savedir.Location = new System.Drawing.Point(157, 110);
+            this.Savedir.Name = "Savedir";
+            this.Savedir.Size = new System.Drawing.Size(100, 20);
+            this.Savedir.TabIndex = 1;
+            // 
+            // Savename
+            // 
+            this.Savename.Location = new System.Drawing.Point(157, 149);
+            this.Savename.Name = "Savename";
+            this.Savename.Size = new System.Drawing.Size(100, 20);
+            this.Savename.TabIndex = 1;
+            // 
+            // ExtensionTextBox
+            // 
+            this.ExtensionTextBox.Location = new System.Drawing.Point(157, 198);
+            this.ExtensionTextBox.Name = "ExtensionTextBox";
+            this.ExtensionTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ExtensionTextBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(279, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(157, 253);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // stopbtn
+            // 
+            this.stopbtn.Location = new System.Drawing.Point(279, 253);
+            this.stopbtn.Name = "stopbtn";
+            this.stopbtn.Size = new System.Drawing.Size(75, 23);
+            this.stopbtn.TabIndex = 2;
+            this.stopbtn.Text = "Stop";
+            this.stopbtn.UseVisualStyleBackColor = true;
+            this.stopbtn.Click += new System.EventHandler(this.stopbtn_Click);
+            // 
+            // Recorder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 320);
+            this.Controls.Add(this.stopbtn);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ExtensionTextBox);
+            this.Controls.Add(this.Savename);
+            this.Controls.Add(this.Savedir);
+            this.Controls.Add(this.Extension);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Recorder";
+            this.Text = "Microphone Recorder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Extension;
+        private System.Windows.Forms.TextBox Savedir;
+        private System.Windows.Forms.TextBox Savename;
+        private System.Windows.Forms.TextBox ExtensionTextBox;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button stopbtn;
+    }
+}
+
---
+++ MicrophoneRecorder/Recorder.cs
@@ -0,0 +1,114 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MicrophoneRecorder
+{
+    public partial class Recorder : Form
+    {
+
+
+
+        private  WaveIn sourceStream;
+        private WaveFileWriter waveWriter;
+        private readonly string FilePath;
+        private readonly string FileName;
+        private readonly int InputDeviceIndex;
+
+
+        public Recorder()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.folderBrowserDialog1.ShowDialog();
+                this.Savedir.Text = this.folderBrowserDialog1.SelectedPath.ToString();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                WaveIn in1 = new WaveIn
+                {
+                    DeviceNumber = this.InputDeviceIndex,
+                    WaveFormat = new WaveFormat(44100, WaveIn.GetCapabilities(this.InputDeviceIndex).Channels)
+                };
+                this.sourceStream = in1;
+                this.sourceStream.DataAvailable += new EventHandler<WaveInEventArgs>(this.SourceStreamDataAvailable);
+                this.waveWriter = new WaveFileWriter(this.Savedir.Text + this.Savename.Text + "." + this.ExtensionTextBox.Text, this.sourceStream.WaveFormat);
+                this.sourceStream.StartRecording();
+                MessageBox.Show("Started");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        [DllImport("winmm.dll", EntryPoint = "mciSendStringA", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+        private static extern int record(string lpstrCommand, string lpstrReturnString, int uReturnLength, int hwndCallback);
+        private void RecordEnd(object sender, EventArgs e)
+        {
+            if (this.sourceStream != null)
+            {
+                this.sourceStream.StopRecording();
+                this.sourceStream.Dispose();
+                this.sourceStream = null;
+            }
+            if (this.waveWriter != null)
+            {
+                this.waveWriter.Dispose();
+                this.waveWriter = null;
+                this.stopbtn.Enabled = false;
+                Application.Exit();
+                Environment.Exit(0);
+            }
+        }
+
+        public void SourceStreamDataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (this.waveWriter != null)
+            {
+                this.waveWriter.Write(e.Buffer, 0, e.BytesRecorded);
+                this.waveWriter.Flush();
+            }
+        }
+
+        private void stopbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MessageBox.Show("Stopped");
+                this.sourceStream.StopRecording();
+                Process.Start(this.Savedir.Text + this.Savename.Text + "." + this.ExtensionTextBox.Text);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+    }
+}
